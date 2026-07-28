b5723fed6e03c804b0538789541529e4c9473c66
Ankush <ankushkarangtad@gmail.com>
2026-07-28 14:58:00 +0530

    fixed some initialisation type errors

--- 01_basics/concepts_tests.cpp
+++ 01_basics/concepts_tests.cpp
@@ -8,8 +8,8 @@ int main(){
 
     /* int x = 3; //copy initialisation
     int x(4); //direct initialisation
-    int x7; //direct list initialisation
-    int x = 7; //copy list initialisation   
+    int x{7}; //direct list initialisation
+    int x = {7}; //copy list initialisation   
     int x; //value initialisation 
     */
     return 0;
