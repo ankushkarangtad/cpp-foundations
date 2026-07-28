573123f99cc285a001e9c3b6a0200bad48aed81c
Ankush <ankushkarangtad@gmail.com>
2026-07-28 16:36:35 +0530

    style: improve console output formatting and readability
    - data_types.cpp: Include <iomanip> and use std::setw() with left-justification to align output columns.
    
    - int_ranges.cpp: clarify print statements by explicitly stating the variable types and the addition operations.

--- 05_data_types/data_types.cpp
+++ 05_data_types/data_types.cpp
@@ -1,29 +1,32 @@
 // A utility program to print the sizes of fundamental C++ data types
 
 #include <iostream>
+#include <iomanip> //for std::setw
 
 void printIntegerSizes(){
-    std::cout<<"Size of int is: "<<sizeof(int)<<'\n';
-    std::cout<<"Size of short int (or short) is: "<<sizeof(short)<<'\n';
-    std::cout<<"Size of long int ( or long) is: "<<sizeof(long)<<'\n';
-    std::cout<<"Size of long long int ( or long long) is: "<<sizeof(long long)<<'\n';
+    std::cout<<std::setw(40)<<"Size of int is: "<<sizeof(int)<<'\n';
+    std::cout<<std::setw(40)<<"Size of short int (or short) is: "<<sizeof(short)<<'\n';
+    std::cout<<std::setw(40)<<"Size of long int (or long) is: "<<sizeof(long)<<'\n';
+    std::cout<<std::setw(40)<<"Size of long long int(or long long) is: "<<sizeof(long long)<<'\n';
 }
 void printBooleanSizes(){
-    std::cout<<"Size of bool is: "<<sizeof(bool)<<'\n';
+    std::cout<<std::setw(40)<<"Size of bool is: "<<sizeof(bool)<<'\n';
 }
 void printCharacterSizes(){
-    std::cout<<"Size of char is: "<<sizeof(char)<<'\n';
-    std::cout<<"Size of wchar_t is: "<<sizeof(wchar_t)<<'\n';
-    std::cout<<"Size of char16_t is: "<<sizeof(char16_t)<<'\n';
-    std::cout<<"Size of char32_t is: "<<sizeof(char32_t)<<'\n';
+    std::cout<<std::setw(40)<<"Size of char is: "<<sizeof(char)<<'\n';
+    std::cout<<std::setw(40)<<"Size of wchar_t is: "<<sizeof(wchar_t)<<'\n';
+    std::cout<<std::setw(40)<<"Size of char16_t is: "<<sizeof(char16_t)<<'\n';
+    std::cout<<std::setw(40)<<"Size of char32_t is: "<<sizeof(char32_t)<<'\n';
 }
 void printFloatingPointSizes(){
-    std::cout<<"Size of float is: "<<sizeof(float)<<'\n';
-    std::cout<<"Size of double is: "<<sizeof(double)<<'\n';
-    std::cout<<"Size of long double is: "<<sizeof(long double)<<'\n';
+    std::cout<<std::setw(40)<<"Size of float is: "<<sizeof(float)<<'\n';
+    std::cout<<std::setw(40)<<"Size of double is: "<<sizeof(double)<<'\n';
+    std::cout<<std::setw(40)<<"Size of long double is: "<<sizeof(long double)<<'\n';
 }
 int main(){
     std::cout<<"\n---------- Size of Data Types (in Bytes): ----------\n\n";
+
+    std::cout<<std::left; //left justify
 
     printIntegerSizes();
     printBooleanSizes();
