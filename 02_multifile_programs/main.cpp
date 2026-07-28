c68d4fab278b2bfd059c78751cf383ad0874a312
Ankush <ankushkarangtad@gmail.com>
2026-07-28 15:22:59 +0530

    fix some typos and add some comments

--- 02_multifile_programs/main.cpp
+++ 02_multifile_programs/main.cpp
@@ -1,6 +1,8 @@
 //not recommended to #include "user_input.cpp" as it cause naming collision , voilate ODR in big program
 #include "user_input.h" // alternatively use forward declaration in small programs
 
+
+// ODR(One Definition Rule) not voilates here because of using hearder guards
 #include "sub.h"
 #include "sub.h"
 
@@ -13,7 +15,7 @@ int main(){
     double num2{user_input()};
     
     std::cout<<"sum = "<<add(num1, num2)<<'\n';
-    std::cout<<"sum = "<<sub(num1, num2)<<'\n';
+    std::cout<<"sub = "<<sub(num1, num2)<<'\n';
 
 
     return 0;   // or use return EXIT_SUCCESS by #include <cstdlib> or <iostream>, if fails , it shows EXIT_FAILURE
--- 02_multifile_programs/sub.h
+++ 02_multifile_programs/sub.h
@@ -1,8 +1,8 @@
-#pragma once // alternate header guard to #ifdef #define #endif
+#pragma once // alternate header guard to #ifndef #define #endif
 
 double sub(double x , double y){
     return x - y ;
 }
 
-
-//#ifdef #define #endif depends on name tag of macro , but #pragma one depends on location of file , if there exist a copy of the same same file , and we #iclude both, then i will cause compile error if they consists of definition.
+//#ifndef #define #endif depends on name tag (Identifier) of macro , but #pragma one depends on location of file
+//if there exist a copy of the same file , and we #include both, then i will cause compile error if they consists of definition.
